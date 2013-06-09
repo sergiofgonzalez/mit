7055c275724d668ccd39d2ba67758f08d245849f
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2013-06-09 17:19:19 +0200

    Added Javadoc and Sonar fixes.

--- mit-samples/001-elements-of-sw-construction/001-hailstone/src/main/java/com/examples/mit/HailstoneApp.java
+++ mit-samples/001-elements-of-sw-construction/001-hailstone/src/main/java/com/examples/mit/HailstoneApp.java
@@ -1,28 +1,56 @@
 package com.examples.mit;
 
-public class HailstoneApp {
+/**
+ * Demonstrates the Hailstone sequence.
+ * @author sergio.f.gonzalez
+ *
+ */
+public final class HailstoneApp {
 
+	/**
+	 * The class constructor marked as private to prevent creation.
+	 */
+	private HailstoneApp() {
+	}
+
+	/**
+	 * The number of times the sequence will be displayed.
+	 */
+	private static final int NUM_TIMES = 10;
+
+	/**
+	 * The main method for the Hailstone Application. Calculates the Hailstone sequence
+	 * for numbers n=0..9
+	 * @param args the arguments received from the command line, which are ignored
+	 */
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.print("Hailstone(" + i + "): ");
+		for (int i = 0; i < NUM_TIMES; i++) {
+			System.out.print("Hailstone(" + i + "): "); //NOSONAR: sergio.f.gonzalez: console app
 			displayHailstoneSequence(i);
 		}
 	}
-	
-	private static void displayHailstoneSequence(int n) {
-		if (n < 1) {
-			System.err.println("Hailstone sequence requires a n > 0");
+
+	/***
+	 * Displays on the standard output/err the Hailstone sequence for a given number.
+	 * @param startingNumber the number from which the Hailstone sequence will be displayed.
+	 * 			startingNumber must be greater than 0 or an error will be displayed.
+	 */
+	private static void displayHailstoneSequence(int startingNumber) {
+		if (startingNumber < 1) {
+			System.err.println("Hailstone sequence requires a startingNumber > 0"); //NOSONAR: sergio.f.gonzalez: console app
 			return;
 		}
-		
+
+		int n = startingNumber;
+
 		while (n != 1) {
-			System.out.print(n + " ");
+			System.out.print(n + " ");	//NOSONAR: sergio.f.gonzalez: console app
 			if (n % 2 == 0) {
 				n = n / 2;
 			} else {
-				n = 3 * n + 1;
+				n = 3 * n + 1; //NOSONAR: sergio.f.gonzalez: part of the sequence
 			}
 		}
-		System.out.println(n);
+		System.out.println(n);	//NOSONAR: sergio.f.gonzalez: console app
 	}
 }
---
+++ mit-samples/001-elements-of-sw-construction/001-hailstone/src/main/java/com/examples/mit/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ * The package for the MIT Examples.
+ * @author sergio.f.gonzalez
+ *
+ */
+package com.examples.mit;
